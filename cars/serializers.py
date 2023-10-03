4ef8517256946ed9019bb09b6ed15d7f4c729446
regvud <silveron.regvud@gmail.com>
2023-10-03 18:27:47 +0300

    less1 done

--- cars/serializers.py
+++ cars/serializers.py
@@ -5,14 +5,11 @@ from cars.models import CarModel
 
 class DynamicFieldsCategorySerializer(serializers.ModelSerializer):
     def __init__(self, *args, **kwargs):
-        # Don't pass the 'fields' arg up to the superclass
         fields = kwargs.pop('fields', None)
 
-        # Instantiate the superclass normally
         super().__init__(*args, **kwargs)
 
         if fields is not None:
-            # Drop any fields that are not specified in the `fields` argument.
             allowed = set(fields)
             existing = set(self.fields)
             for field_name in existing - allowed:
