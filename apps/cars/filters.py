605680e4aba1c253f326fee7a8ce3c16eb4230b7
regvud <silveron.regvud@gmail.com>
2023-10-05 10:39:55 +0300

    less2 filters

--- apps/cars/filters.py
+++ apps/cars/filters.py
@@ -1,2 +1,15 @@
-def car_filter():
-    pass
+from django.db.models import QuerySet
+from django.http import QueryDict
+
+from apps.cars.models import CarModel
+
+
+def car_filter(cars: QueryDict) -> QuerySet:
+    qs = CarModel.objects.all()
+
+    for k, v in cars.items():
+        match k:
+            case 'year_lt':
+                qs = qs.filter(year__lt=v)
+
+    return qs
--- apps/cars/views.py
+++ apps/cars/views.py
@@ -1,12 +1,16 @@
 from rest_framework import generics
 
+from apps.cars.filters import car_filter
 from apps.cars.models import CarModel
 from apps.cars.serializers import CarSerializer
 
 
 class CarsListCreateView(generics.ListCreateAPIView):
-    queryset = CarModel.objects.all()
+    # queryset = CarModel.objects.all()
     serializer_class = CarSerializer
+
+    def get_queryset(self):
+        return car_filter(self.request.query_params)
 
 
 class CarRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
